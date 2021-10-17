d7c2d3c556d36dc3ac93986bb932415b7f61009f
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-10-17 21:10:59 +0900

    更新用のリンクを追加しました。

--- list.php
+++ list.php
@@ -1,19 +1,46 @@
-<?php
-    // 他のファイルを読み込む
-    require_once 'function.php'; 
-    try {
-        // インスタンス生成
-        $dbh = db_open();
-        // SQL
-        $sql = "SELECT title, author FROM books";
-        $statement = $dbh->query($sql);
-        // 取得した結果について1行ずつ処理
-        while ($row = $statement->fetch()) {
-            echo "書籍名：" . str2html($row[0]) . "<br>";
-            echo "著者名：" . str2html($row[4]) . "<br><br>";
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <link rel='stylesheet' type='text/css' href="./style.css">
+    </head>
+    <body>
+        <header>
+            <h1>書籍データベース</h1>
+        </header>
+        <?php
+        // 他のファイルを読み込む
+        require_once 'function.php'; 
+        try {
+            // インスタンス生成
+            $dbh = db_open();
+            // SQL
+            $sql = "SELECT title, author FROM books";
+            $statement = $dbh->query($sql);
+        ?>
+        <table>
+            <tr>
+                <th>更新</th>
+                <th>書籍名</th>
+                <th>ISBN</th>
+                <th>価格</th>
+                <th>出版日</th>
+                <th>著者名</th>
+            </tr>
+            <?php while ($row = $statement->fetch()) :?>
+                <td><a href="edit.php?id=<?php echo (int) $row['id']; ?>">更新</a></td>
+                <td><?php echo str2html($row['title']) ?></td>
+                <td><?php echo str2html($row['isbn']) ?></td>
+                <td><?php echo str2html($row['price']) ?></td>
+                <td><?php echo str2html($row['publish']) ?></td>
+                <td><?php echo str2html($row['author']) ?></td>
+            <?php endwhile; ?>
+        </table>
+        <?php    
+        } catch (PDOException $e) {
+            echo "エラー!：" . $e->getMessage() . "<br>";
+            exit;
         }
-    } catch (PDOException $e) {
-        echo "エラー!：" . $e->getMessage() . "<br>";
-        exit;
-    }
-?>
+        ?>
+    </body>
+</html>
