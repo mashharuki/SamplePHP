581bfe860c233135e71e5b352a9c21696c694a32
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-10-17 20:10:32 +0900

    DBからのデータを表示できるように修正

--- list.php
+++ list.php
@@ -1,4 +1,6 @@
 <?php
+    // 他のファイルを読み込む
+    require_once 'function.php'; 
     try {
         // ユーザーIDとパスワードの設定
         $user = "phpuser";
@@ -16,7 +18,14 @@
             $password,
             $opt
         );
-        var_dump($dbh);
+        // SQL
+        $sql = "SELECT title, author FROM books";
+        $statement = $dbh->query($sql);
+        // 取得した結果について1行ずつ処理
+        while ($row = $statement->fetch()) {
+            echo "書籍名：" . str2html($row[0]) . "<br>";
+            echo "著者名：" . str2html($row[4]) . "<br><br>";
+        }
     } catch (PDOException $e) {
         echo "エラー!：" . $e->getMessage() . "<br>";
         exit;
