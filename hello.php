7dc6390775f35b45fc1eec792c7de11b68dc71e6
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-10-17 16:18:52 +0900

    追記

--- hello.php
+++ hello.php
@@ -6,7 +6,17 @@
         <b>
             <?php
                 $greeting = "こんにちは";
+                $price = 1000;
                 echo $greeting;
+                var_dump($greeting);
+                var_dump($price);
+                $a = "文字列1";
+                $b = "文字列2";
+                echo $a . $b;
+                $text = "こんにちは";
+                $text .= "今日の天気は";
+                $text .= "いい天気です。";
+                echo $text;
             ?>
         </b>
     </body>
