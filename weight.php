62e8d0f4c816f7c1efd3d201c2fed6811f321b7a
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-10-17 17:21:06 +0900

    追加機能実装完了

--- weight.php
+++ weight.php
@@ -1,8 +1,23 @@
 <?php
+    require_once 'function.php';
+    // 身長と体重を取得する。
     $height = (float) $_POST['height'];
-    if ((0 < $height) && ($height < 3)) {
-        echo '適正体重は'. $height * $height * 22 . "kgです";
-    } else {
-        echo '身長を正しく入力してください。';
+    $weight = (float) $_POST['weight'];
+    // 体重と身長の入力値に問題が無いかチェックする。
+    if (( 0 < $height ) && ( $height < 3 )) {
+        echo "身長を正しく入力してください。";
+        exit;
+    } 
+    if (( $weight < 50 ) || ( 200 < $weight )) {
+        echo "体重を正しく入力してください。";
+        exit;
     }
+    // 適正体重の算出
+    $goal_weight = $height * $height * 22;
+    // 適正体重までの差を算出
+    $difference = abs($goal_weight - $weight);
+
+    echo "体重" . str2html($weight) . "kg<br>";
+    echo "理想" . str2html($goal_weight) . "kg<br>";
+    echo '後' . str2html($difference) . 'kgで適正体重です。 <br>';
 ?>
